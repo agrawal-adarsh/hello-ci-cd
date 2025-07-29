7c5b03ca4b0f6a4492aab8434efb53d0daaf835f
aadarsh <aadarsh@cdac.in>
2025-07-29 10:32:53 +0530

    Intial Commit

---
+++ src/main/java/com/example/hellocicd/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.hellocicd;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello CICD";
+    }
+    
+}
---
+++ src/test/java/com/example/hello_ci_cd/HelloCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
