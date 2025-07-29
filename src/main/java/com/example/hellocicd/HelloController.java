6f142204652a57f45eb11a90180a0e6bdf54bdb4
agrawal-adarsh <adarshagrawal968@gmail.com>
2025-07-29 14:35:55 +0530

    lets fail test

--- src/main/java/com/example/hellocicd/HelloController.java
+++ src/main/java/com/example/hellocicd/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello CICD";
+        return "Hello CICD c";
     }
     
 }
