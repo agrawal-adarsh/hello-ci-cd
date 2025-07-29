2a09d3c74486d2b43caf1f8df28087d35727eafa
aadarsh <aadarsh@cdac.in>
2025-07-29 11:00:32 +0530

    Fix test config for CI

--- src/test/java/com/example/hello_ci_cd/HelloCiCdApplicationTests.java
+++ src/test/java/com/example/hello_ci_cd/HelloCiCdApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.hello_ci_cd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.hellocicd.HelloCiCdApplication;
+
+@SpringBootTest(classes = HelloCiCdApplication.class)
 class HelloCiCdApplicationTests {
 
 	@Test
